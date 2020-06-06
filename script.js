c551ebd9e4fa6c8e25081c3bfbe16c689b1af46c
Sachin Yadav <sachinyadav7024@gmail.com>
2020-06-07 01:38:48 +0530

    create basic grid board

---
+++ script.js
@@ -0,0 +1,7 @@
+numRows = 5;
+numCols = 5;
+//removing the top and left borders for removal
+$('#board > .cell:nth-child(-n+' + numCols).css("border-top","none");
+$('#board > .cell:nth-child('+ numRows + 'n+1').css("border-left","none");
+
+
