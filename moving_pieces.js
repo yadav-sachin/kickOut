d7a0ba2d3b5c8f3b879163e989b91027360da1a1
Sachin Yadav <sachinyadav7024@gmail.com>
2020-06-07 17:48:26 +0530

    implement kicking out a token

--- moving_pieces.js
+++ moving_pieces.js
@@ -4,11 +4,16 @@ $(function(){
     //First click for selecting the piece to move
     // Creating the click event for a cell that contains a piece
     $('#board').on('click', '.contains_piece',function () {
+        //if some token was already selected 
+        if ( $('.selected_piece').length ){
+            $('.selected_piece').removeClass('selected_piece');
+            $(".valid_move_cell").removeClass('valid_move_cell');
+        }
         $(this).addClass('selected_piece');
         selectedCellID = $(this).attr('id');
         let cellRow = parseInt(selectedCellID[0]), cellCol = parseInt(selectedCellID[2]);
-        for (let i = 1; i <= cellRow; ++i)
-            for (let j = 1; j <= cellCol; ++j)
+        for (let i = 0; i <= cellRow; ++i)
+            for (let j = 0; j <= cellCol; ++j)
             {
                 if (i == cellRow && j == cellCol) continue;
                 let $cell = $('#' + i + '_' + j);
@@ -20,11 +25,17 @@ $(function(){
         console.log("A valid cell was clicked");
         $('#' + selectedCellID).removeClass('contains_piece');
         $('#' + selectedCellID).html("");
-        $(this).addClass('contains_piece');
         $(".valid_move_cell").removeClass('valid_move_cell');
         $('.selected_piece').removeClass('selected_piece');
-        $('.contains_piece').html("&#9679;");
+        // If the clicked cell is not an outside cell
+        if ($(this).hasClass('out_cell') == false) 
+        {
+            $(this).addClass('contains_piece');
+            $('.contains_piece').html("&#9679;");
+        }
     });
 
+    $('body').click( function(){
 
+    });
 });
---
+++ setup_board.js
@@ -0,0 +1,13 @@
+$(function () {
+
+    numRows = 5;
+    numCols = 5;
+    //removing the top and left borders for removal
+    for (let j = 1; j <= numCols; ++j)
+        $('#1_'+ j).addClass('top_cell');
+    for (let i = 1; i <= numRows; ++i)
+        $('#'+ i + '_1').addClass('left_cell');
+    $('#board > .cell.contains_piece').html("&#9679;");
+});
+
+
