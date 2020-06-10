ee72a6e3c8ded171fc90c03387d943ebff6bf682
Sachin Yadav <sachinyadav7024@gmail.com>
2020-06-11 01:13:48 +0530

    config server port and engines for Heroku

--- app.js
+++ app.js
@@ -6,12 +6,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'hbs');
 
+const serverPort = process.env.PORT || 3000;
 app.use('/public', express.static('public'));
 
 app.get('/', (req, res) => {
     res.render('index');
 });
 
-app.listen(3000, () => {
-    console.log('Server started at http://localhost:3000');
+app.listen(serverPort, () => {
+    console.log('Server started at http://localhost:'+ serverPort);
 })
