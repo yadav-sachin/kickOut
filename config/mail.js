2471fbeff752a171e7eee4521dfb2bb4c733b6f9
Sachin Yadav <sachinyadav7024@gmail.com>
2020-06-13 17:46:33 +0530

    remove signup mail bugs

--- config/mail.js
+++ config/mail.js
@@ -1,53 +1,41 @@
 const sgMail = require('@sendgrid/mail');
 require('dotenv').config();
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const chalk = require('chalk');
 
-function sendVerification(username,firstname, email, code){
-    try{
-        const msg = {
-            to: email,
-            from: process.env.MAIL_ID,
-            subject: 'Complete Account Verification',
-            text:`Thanks for Signing up ${firstname}.
-            Please verify your mail to get access to ton of fun in our Multiplayer Games.
-            Thank You!
-            Your Verification Code is ${code}.`,
-            html: `<h3>Thanks for Signing up ${firstname} !</h3>
-            <p>Please verify your mail to get access to ton of fun in our Multiplayer Game. The link will expire in 24 hours.</p>
-            <p style = "color: yellow;"><strong>Thank You!</strong></p>
-            <br>
-            <a href="https://kick-out.herokuapp.com/user/verify/${username}/${code}">Proceed Here to Verify</a>`
-        }
-        sgMail.send(msg);
-        return true;
-    }catch(err)
-    {
-        console.error(err);
-        return false;
+async function sendVerification(username,firstname, email, code){
+    const msg = {
+        to: email,
+        from: process.env.EMAIL_ID,
+        subject: 'Complete Account Verification',
+        text:`Thanks for Signing up ${firstname}.
+        Please verify your mail to get access to ton of fun in our Multiplayer Games.
+        Thank You!
+        Your Verification Code is ${code}.`,
+        html: `<h3>Thanks for Signing up ${firstname} !</h3>
+        <p>Please verify your mail to get access to ton of fun in our Multiplayer Game. The link will expire in 24 hours.</p>
+        <p style = "color: yellow;"><strong>Thank You!</strong></p>
+        <br>
+        <a href="https://kick-out.herokuapp.com/user/verify/${username}/${code}">Proceed Here to Verify</a>`
     }
+    await sgMail.send(msg);
 }
 
-function forgotPassword(username,firstname, email, code){
-    try{
-        const msg = {
-            to: email,
-            from: process.env.MAIL_ID,
-            subject: 'Password Recovery',
-            text:`Thanks for Signing up ${firstname}.
-            Please verify your mail to get access to ton of fun in out Multiplayer Games.
-            Thank You!
-            Your Verification Code is ${code}.`,
-            html: `<h3>You requested for password change ${firstname} ?</h3>
-            <p>Proceed to the link below to reset your password. The link will expire in 24 hours.</p>
-            <br>
-            <a href="https://kick-out.herokuapp.com/user/resetpassword/${username}/${code}">Proceed Here to Verify</a>`
-        }
-        sgMail.send(msg);
-        return true;
-    }catch(err)
-    {
-        return false;
+async function forgotPassword(username,firstname, email, code){
+    const msg = {
+        to: email,
+        from: process.env.EMAIL_ID,
+        subject: 'Password Recovery',
+        text:`Thanks for Signing up ${firstname}.
+        Please verify your mail to get access to ton of fun in out Multiplayer Games.
+        Thank You!
+        Your Verification Code is ${code}.`,
+        html: `<h3>You requested for password change ${firstname} ?</h3>
+        <p>Proceed to the link below to reset your password. The link will expire in 24 hours.</p>
+        <br>
+        <a href="https://kick-out.herokuapp.com/user/resetpassword/${username}/${code}">Proceed Here to Verify</a>`
     }
+    await sgMail.send(msg);
 }
 
 module.exports = {
--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize');
 const chalk = require('chalk');
 const router = express.Router();
 const { User } = require('./../models/User');
-const sgMail = require('./../config/mail');
+const Mail = require('./../config/mail');
 
 router.get('/', (req, res) => {
     res.render('login');
@@ -21,8 +21,7 @@ router.post('/login', async (req, res) => {
 router.get('/verify/:username/:code', async (req, res) => {
     try {
         const user = User.verify(req.params.username, req.params.code);
-        user.
-            res.send('Set Your Password');
+        res.send('Set Your Password');
     } catch (err) {
         res.send({ error: err });
     }
@@ -32,8 +31,8 @@ router.post('/signup', async (req, res) => {
     try {
         const newUser = await User.create(req.body);
         res.send(newUser);
-        sgMail.sendVerification(newUser.username, newUser.first_name, newUser.email, newUser.verification_code);
-        console.log(chalk.green('User created successfully'));
+        const isSent = await Mail.sendVerification(newUser.username, newUser.first_name, newUser.email, newUser.verification_code);
+        console.log(chalk.green('User created and sent Verification Mail successfully'));
     } catch (err) {
         res.send({ error: 'User Cannot Be Created' });
     }
