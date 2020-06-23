a995e5a306461665cb54ede5545768f8a0cc5f6d
Sachin Yadav <sachinyadav7024@gmail.com>
2020-06-24 02:09:37 +0530

    all user functions: verify changepassword complete

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,10 +9,10 @@ exports.signupUser = async (req, res) => {
         req.flash('success', 'User Created Successfully');
         await sgMail.sendVerification(user);
         req.flash('success', 'Verification Mail sent successfully to the given email. The mail may shifted to Promotions/Span Category.');
-        req.flash('success','Verification Code will expire in 6 hours');
+        req.flash('success', 'Verification Code will expire in 6 hours');
         res.status(201).render('login');
     } catch (err) {
-        const formData = {email, firstName, lastName, username} = req.body;
+        const formData = { email, firstName, lastName, username } = req.body;
         req.flash('formData', formData);
         req.flash('error', err.message);
         res.redirect('/user/login');
@@ -25,42 +25,54 @@ exports.verifyUser = async (req, res) => {
     try {
         console.log(username, verificationCode);
         const user = await User.findOne({ username, verificationCode });
-        if (!user)
+        if (!user) {
+            req.flash('error', 'Invalid Username');
             throw new Error('Invalid Username');
+        }
         const currTime = Date.now(), allowedTime = 6 * 60 * 60 * 1000; //6 hours
         if (currTime - user.verificationRequestTime > allowedTime) {
             await User.findByIdAndDelete(user._id);
-            throw new Error('Verified Code Expired. Try Registering Again');
+            {
+                req.flash('error', 'Verified Code Expired. Try Registering Again');
+                throw new Error('Verified Code Expired. Try Registering Again');
+            }
         }
-        const token = await user.generateAuthToken();
-        user.authTokens.push({token});
+        user.isVerified = true;
+        req.flash('success', 'User Successfully Verified');
+        user.password = 'password';
+        req.flash('info', "Current Password is Set to 'password'. Change the Password Now");
         await user.save();
-        res.redirect('/user/setPassword');
-        console.log(chalk.green('User ' + username + ' Successfully got Verification Token. Will Be asked to set Password Now'));
+        console.log(chalk.green('User ' + username + ' Successfully got Verification. Will Be asked to set Password Now'));
+        req.login(user, function (err) {
+            if (err) { return next(err); }
+            res.redirect('/user/changePassword');
+        });
     } catch (err) {
         console.log(chalk.red(username, ' Trouble while verifying token', err));
-        err+= '. Could Not Be Verified';
-        res.redirect('/user/login/?error='+encodeURIComponent(err));
+        req.flash('error', err.message);
+        res.redirect('/user/login');
     }
 }
 
 
-exports.setPassword = async (req, res) => {
-    const errorLogs = [];
+exports.changePassword = async (req, res) => {
     try {
         req.body.password = req.body.password.trim();
         req.body.password2 = req.body.password2.trim();
         if (req.body.password !== req.body.password2)
+        {
+            req.flash('error', 'The two passwords do not match. Try Again');
             throw new Error('The two inputs do not match');
-        const user = req.body.user;
-        if (!user)
-            throw new Error('Invalid User');
-        user.isVerified = true;
+        }
+        const user = req.user;
         user.password = req.body.password;
         await user.save();
-        res.redirect('/user/dashboard');
+        req.flash('success', 'Password Successfully Changed');
+        req.logout();
+        req.flash('info', 'Login with the new password');
+        res.redirect('/user/login');
     } catch (err) {
-        errorLogs.push(err);
-        res.render('setPassword', { errorLogs });
+        req.flash('error', err.message);
+        res.redirect('/user/changepassword');
     }
 }
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -9,13 +9,13 @@ router.get('/', (req, res) => res.redirect('/user/login'));
 router.get('/signup', redirectAuthenticated, (req, res) => res.redirect('/user/login'));
 router.get('/login', redirectAuthenticated, (req, res) => res.render('login'));
 router.get('/verify/:username/:verificationCode', redirectAuthenticated, userController.verifyUser);
-router.get('/setpassword', isAuthenticated, (req, res) => res.render('setPassword'));
+router.get('/changepassword', isAuthenticated, (req, res) => res.render('changePassword'));
 router.get('/dashboard',isAuthenticated,  (req, res) =>{ res.render('dashboard', {username: req.user.username}); });
 router.get('/logout', isAuthenticated, (req, res)=> { req.logout(); res.redirect('/'); })
 
 // POST Requests
 router.post('/login',passport.authenticate('local', { successRedirect: '/user/dashboard', failureFlash: true, failureRedirect: '/user/login' }));
 router.post('/signup', redirectAuthenticated, userController.signupUser);
-router.post('/setpassword', isAuthenticated, userController.setPassword);
+router.post('/changepassword', isAuthenticated, userController.changePassword);
 
 module.exports = router;
