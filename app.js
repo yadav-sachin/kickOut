9ea37113e860ecb6c77e12d0ff36d21282fda8b4
Sachin Yadav <sachinyadav7024@gmail.com>
2020-06-23 18:52:43 +0530

    fix passportjs login password match

--- app.js
+++ app.js
@@ -2,21 +2,22 @@ const express = require('express');
 require('dotenv').config();
 require('./config/db');
 const session = require('express-session');
-const flash = require('connect-flash');
+const flash = require('express-flash');
 const hbs = require('hbs');
 const path = require('path');
 const passport = require('passport');
 const indexRouter = require('./routers/index');
 
 const app = express();
+require('./config/passport')(passport);
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));    
-app.set('trust proxy' , 1);
+app.use(express.urlencoded({ extended: true }));
+app.set('trust proxy', 1);
 app.use(session({
-    secret: process.env.SESSION_SECRET_KEY, 
+    secret: process.env.SESSION_SECRET_KEY,
     resave: false,
     saveUninitialized: true,
-    cookie: {secure: true}
+    cookie: { secure: true }
 }));
 // app.use(express.cookieParser('SESSION_SECRET_KEY'));
 app.use(flash());
@@ -30,7 +31,7 @@ hbs.registerPartials(path.join(__dirname + '/views/partials'));
 app.use('/', indexRouter);
 
 
-const serverPort = process.env.PORT || 3000;
+const serverPort = process.env.PORT || 9000;
 app.listen(serverPort, () => {
     console.log('Server started ' + serverPort);
 });
--- config/passport.js
+++ config/passport.js
@@ -1,33 +1,42 @@
-const mongoose = require('mongoose');
-const passport = require('passport');
 const localStrategy = require('passport-local').Strategy;
 const User = require('./../models/userModel');
 const chalk = require('chalk');
+const bcrypt = require('bcrypt');
 
-passport.use(new localStrategy({ usernameField: 'email', passwordField: 'password'}, async function (email, password, done) {
-    try {
-        console.log('Login request for', email);    
-        const user = await User.find({ email });
-        if ((!user) || (!user.isVerified))
-            return done(null, false, { message: 'No registered Username/Email found' });
-        const isMatch = await bcrypt.compare(password, user.password);
-        if (!isMatch)
-            {
-                req.flash('errorLogs' ['Password Incorrect']);
-                return done(null, false);
+const configPassport = function (passport) {
+    const authenticateUser = async function (email, password, done) {
+        try {
+            console.log('Login request for', email);
+            const user = await User.findOne({ email });
+            console.log(user);
+            console.log(user.isVerified);
+            if ((!user) || (!user.isVerified))
+                return done(null, false, { message: 'No registered Username/Email found' });
+            const isMatch = await bcrypt.compare(password, user.password);
+            if (!isMatch) {
+                // req.flash('errorLogs'['Password Incorrect']);
+                console.log(chalk.blue('Password Incorrect for login by', email));
+                return done(null, false, {message: 'Password Incorrect' });
             }
-        return done(null, user);
-    } catch (err) {
-        done(err, null);
+            console.log(chalk.yellow('Password Correct for login by', email));
+            return done(null, user);
+        } catch (err) {
+            console.log(chalk.red('Some error occurred for login by', email, err));
+            done(err, null);
+        }
     }
-}));
+    passport.use(new localStrategy({ usernameField: 'email', passwordField: 'password' }, authenticateUser));
 
-passport.serializeUser(function (user, done) {
-    done(null, user.id);
-});
-
-passport.deserializeUser(function (id, done) {
-    User.findById(id, function (err, user) {
-        done(err, user);
+    passport.serializeUser(function (user, done) {
+        done(null, user.id);
     });
-});
+
+    passport.deserializeUser(function (id, done) {
+        User.findById(id, function (err, user) {
+            done(err, user);
+        });
+    });
+
+}
+
+module.exports = configPassport;
